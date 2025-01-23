fix: fix some errors and softmax
Fixed the search of the best path and the formula of the softmax

monte_carlo.py
@@ -1,10 +1,10 @@
 # monte_carlo.py
-import random
+import numpy as np
 from constants import X_SYMBOL, O_SYMBOL
 from game_functions import check_win, check_draw
 from machine import get_random_move
 
-def monte_carlo_simulation(board, player, board_size=3, num_simulations=100):
+def monte_carlo_simulation(board, player, board_size=3, original=True, original_player = " ", move_values = []):
   """
   Performs Monte Carlo simulations to estimate the value of each possible move.
 
@@ -17,57 +17,54 @@ def monte_carlo_simulation(board, player, board_size=3, num_simulations=100):
   Returns:
     list: one-hot vector with the calculated value for every possible move.
   """
-  move_values = [0] * (board_size * board_size)
+  if original:
+    original_player = player
+    move_values = [0] * (board_size * board_size)
   
-  for _ in range(num_simulations):
-    # Iterate over each possible move
-    for move_index in range(board_size * board_size):
-      if board[move_index] == " ":
-        # breakpoint()
-        #Make a copy of the board and play the current move
-        temp_board = list(board)
-        # breakpoint()
-        temp_board[move_index] = player
-        # breakpoint()
+  # Iterate over each possible move
+  for move_index in range(board_size * board_size):
+    if board[move_index] == " ":
+      breakpoint()
+      #Make a copy of the board and play the current move
+      temp_board = list(board)
+      # breakpoint()
+      temp_board[move_index] = player
+      # breakpoint()
 
-        # Check if this move is winner
-        if check_win(temp_board, player, board_size, board_size):
+      # Check if this move is winner
+      if check_win(temp_board, player, board_size, board_size):
+        if player == original_player:
           move_values[move_index] += 1
-          # Check if this move is a draw
-        elif check_draw(temp_board):
-          pass
         else:
-          # Play a random game starting with that move
-          winner = play_random_game(temp_board, X_SYMBOL if player == O_SYMBOL else O_SYMBOL, board_size)
-          # breakpoint()
+          move_values[move_index] -= 1
+      # Check if this move is a draw
+      elif check_draw(temp_board):
+        pass
+      else:
+        monte_carlo_simulation(temp_board, O_SYMBOL if player == X_SYMBOL else X_SYMBOL, board_size, False, original_player, move_values)
 
-          # Update move value based on the outcome.
-          if winner == player:
-            move_values[move_index] +=1
-          elif winner == None: #Draw
-            pass
-          else:
-            move_values[move_index] -=1
-  
-  # Calculate probabilities
-  total_simulations = num_simulations
-  for i in range(len(move_values)):
-    if move_values[i] !=0 :
-      move_values[i] = (move_values[i] / total_simulations) #We get the probability
-    else:
-      move_values[i] = 0
+  if original:
+    return calculate_probabilities(move_values)
 
-  #Normalize data to create probabilities with softmax
-  max_value = max(move_values)
-  min_value = min(move_values)
+def calculate_probabilities(move_values):
+  """
+  Calculates the probabilities of winning for each move with softmax.
 
-  if max_value == min_value:
-    move_probabilities = [1/ (board_size * board_size) ] * (board_size * board_size)
-  else:
-    move_probabilities = [(value - min_value) / (max_value - min_value) for value in move_values]
+  Args:
+    move_values (list): The accumulated values for each move.
 
-
+  Returns:
+    list: The probabilities of winning for each move.
+  """
+  breakpoint()
+  z = np.array(move_values)
+  beta = 1
+  move_probabilities = np.exp(beta * z) / np.sum(np.exp(beta * z))
   return move_probabilities
+  # z_exp = [math.exp(i) for i in move_values]
+  # sum_z_exp = sum(z_exp)
+  # move_probabilities = [round(i / sum_z_exp, 3) for i in z_exp]
+  # breakpoint()
 
 def play_random_game(board, current_player, board_size):
   """
@@ -93,10 +90,10 @@ def play_random_game(board, current_player, board_size):
 
 if __name__ == '__main__':
   # Example usage
-  initial_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  current_player = X_SYMBOL
-  probabilities = monte_carlo_simulation(initial_board, current_player)
-  print("Probabilities of win for each move",probabilities)
+  # initial_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  # current_player = X_SYMBOL
+  # probabilities = monte_carlo_simulation(initial_board, current_player)
+  # print("Probabilities of win for each move",probabilities)
 
   initial_board = ["X", "O", "O",
                     "X", "O", "X",